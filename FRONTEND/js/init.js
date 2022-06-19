0e8788c7029084514cda8a67fc3acd4c6c9fdb7c
Samuel Lopes <samek.vitor1@gmail.com>
2022-06-19 03:50:40 -0300

    mascara de cpf e cnpj

--- FRONTEND/js/init.js
+++ FRONTEND/js/init.js
@@ -1,7 +1,38 @@
+
 (function($){
   $(function(){
-
+    M.AutoInit();
     $('.sidenav').sidenav();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+$(document).ready(function(){	
+  $("#cnpj").mask("99.999.999/9999-99");
+});
+
+$("#cnpj").on("blur", function(){
+  let cnpj_value = $(this).val();
+
+  if(jsbrasil.validateBr.cnpj(cnpj_value)) {
+      alert("cnpj Válido");
+  } else {
+    alert("cnpj inválido")
+  }
+});
+
+
+$(document).ready(function(){	
+  $("#cpf").mask("999.999.999-99");
+});
+
+$("#cpf").on("blur", function(){
+  let cpf_value = $(this).val();
+
+  if(jsbrasil.validateBr.cpf(cpf_value)) {
+      alert("cpf Válido");
+  } else {
+    alert("cpf inválido")
+  }
+});
