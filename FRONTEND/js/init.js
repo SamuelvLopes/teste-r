44ff525865e627ddf86f6c15624943ec092646af
Samuel Lopes <samek.vitor1@gmail.com>
2022-06-19 19:29:33 -0300

    auto preenchimento funcionando

--- FRONTEND/js/init.js
+++ FRONTEND/js/init.js
@@ -7,3 +7,26 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+
+
+  var dados={};
+  const node = document.getElementById('autocomplete-input');
+        node.addEventListener("keyup", function(event) {
+         window.dados={};
+        
+    atualizar_pesquisa_cliente(document.getElementById('autocomplete-input').value);
+    document.getElementById('autocomplete-input').click();
+    if(event.key=='Enter'){
+        pesquisar_cliente(document.getElementById('autocomplete-input').value);
+        return;
+    }   
+    $('input.autocomplete').autocomplete({
+       
+         data: dados
+       });
+       document.getElementById('autocomplete-input').click();
+       
+});
+
+
+
