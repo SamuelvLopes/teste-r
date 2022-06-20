70951a0fb8e22457487deeb2c26729a2c46b2346
Samuel Lopes <samek.vitor1@gmail.com>
2022-06-20 05:22:15 -0300

    demostrando lib de idioma

--- app/application/controllers/Home.php
+++ app/application/controllers/Home.php
@@ -1,18 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Teste extends CI_Controller {
 
 	public function index()
 	{
 		
-		$this->load->view('welcome_message');
-		foreach($this->connection->query("SELECT * FROM pao") as $linha){
-		
-				//var_dump($this -> lang -> line('title'));
-				echo'<hr>';
-
-				}
+		$this->dtemplate->show('home');
                                 
 	}
 }
